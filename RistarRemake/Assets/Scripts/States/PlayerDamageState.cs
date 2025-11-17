d033bb959fcb52f82f19595b58e1187fdc089c0a
RemyMasselot <masselotremy@gmail.com>
2025-11-17 13:47:08 +0100

    Leap State Orientation

--- RistarRemake/Assets/Scripts/States/PlayerDamageState.cs
+++ RistarRemake/Assets/Scripts/States/PlayerDamageState.cs
@@ -14,7 +14,7 @@ public class PlayerDamageState : PlayerBaseState
         if (_player.LifesNumber > 0)
         {
             //_player.PlayerRigidbody.gravityScale = 1;
-            _player.PlayerRigidbody.velocity = new Vector2(0, _player.LeapForceV/1.5f);
+            //_player.PlayerRigidbody.velocity = new Vector2(0, _player.LeapForceV/1.5f);
 
             _player.Invincinbility.InvincibilityCounter = _player.Invincinbility.InvincibilityTime;
             _player.Invincinbility.IsInvincible = true;
--- RistarRemake/Assets/Scripts/States/PlayerLeapState.cs
+++ RistarRemake/Assets/Scripts/States/PlayerLeapState.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 public class PlayerLeapState : PlayerBaseState
@@ -5,8 +6,6 @@ public class PlayerLeapState : PlayerBaseState
     public PlayerLeapState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory)
     : base(currentContext, playerStateFactory) { }
 
-    private float startLeapPositionY;
-    private bool newOrientation;
     private float currentTimeLeapCurve = 0f;
     private Vector3 leapStartPosition;
 
@@ -14,29 +13,19 @@ public class PlayerLeapState : PlayerBaseState
     {
         //Debug.Log("JUMP ENTER");
 
-        //_player.PlayerRigidbody.gravityScale = 1;
-
-        // animation curve
-        // position XY selon le temps
-        // Temps du leap
-        // Set player.veliocity to animation curve value
-
         leapStartPosition = _player.transform.position;
-
-        //_player.PlayerRigidbody.velocity = new Vector2(0, _player.LeapForceV);
-
-        //startLeapPositionY = _player.transform.position.y;
-        //newOrientation = false;
     }
 
     public override void UpdateState()
     {
-        _player.PlayerDirectionVerif();
-
         SetPlayerPosition();
+
+        _player.PlayerDirectionVerif();
 
         CheckSwitchStates();
     }
+
+    public override void FixedUpdateState() { }
 
     private void SetPlayerPosition()
     {
@@ -48,47 +37,29 @@ public class PlayerLeapState : PlayerBaseState
             currentTimeLeapCurve += Time.deltaTime;
             float curveTime = Mathf.Clamp01(currentTimeLeapCurve / _player.TimeToLeap);
             float curveValue = _player.LeapCurve.Evaluate(curveTime); // renvoie une valeur entre 0 et 1
-            _player.transform.position = leapStartPosition + new Vector3(-curveTime * _player.LeapForce, curveValue * _player.LeapForce / 2, _player.transform.position.z);
+
+            float moveValueH = _player.MoveH.ReadValue<float>();
+            if (moveValueH < 0)
+            {
+                _player.transform.position = leapStartPosition + new Vector3(-curveTime * _player.LeapForce, curveValue * _player.LeapForce / 2, _player.transform.position.z);
+            }
+            else if (moveValueH > 0)
+            {
+                _player.transform.position = leapStartPosition + new Vector3(curveTime * _player.LeapForce, curveValue * _player.LeapForce / 2, _player.transform.position.z);
+            }
+            else if (_player.IsPlayerTurnToLeft)
+            {
+                _player.transform.position = leapStartPosition + new Vector3(-curveTime * _player.LeapForce, curveValue * _player.LeapForce / 2, _player.transform.position.z);
+            }
+            else if (_player.IsPlayerTurnToLeft == false)
+            {
+                _player.transform.position = leapStartPosition + new Vector3(curveTime * _player.LeapForce, curveValue * _player.LeapForce / 2, _player.transform.position.z);
+            }
             //Debug.Log("X : " + curveTime + "Y : " + curveValue);
         }
         else
         {
             SwitchState(_factory.Fall());
-        }
-    }
-
-    public override void FixedUpdateState()
-    {
-        //if (_player.transform.position.y >= startLeapPositionY + 1.5f)
-        //{
-        //    if (newOrientation == false)
-        //    {
-        //        newOrientation = true;
-        //        if (_player.IsPlayerTurnToLeft == false)
-        //        {
-        //            _player.PlayerRigidbody.velocity = new Vector2(_player.LeapForceH, _player.LeapForceV / 2);
-        //        }
-        //        else
-        //        {
-        //            _player.PlayerRigidbody.velocity = new Vector2(-_player.LeapForceH, _player.LeapForceV / 2);
-        //        }
-        //    }
-
-        //    AirControl();
-        //}
-    }
-
-    private void AirControl()
-    {
-        float moveValueH = _player.MoveH.ReadValue<float>();
-        float moveValueV = Mathf.Clamp(_player.MoveV.ReadValue<float>(), _player.InputFallSpeedIncrease, _player.InputFallSpeedDecrease);
-        if (moveValueH != 0)
-        {
-            _player.PlayerRigidbody.velocity = new Vector2(moveValueH * _player.HorizontalMovementMultiplier, _player.PlayerRigidbody.velocity.y + moveValueV);
-        }
-        else
-        {
-            _player.PlayerRigidbody.velocity = new Vector2(_player.PlayerRigidbody.velocity.x, _player.PlayerRigidbody.velocity.y + moveValueV);
         }
     }
 
@@ -111,12 +82,6 @@ public class PlayerLeapState : PlayerBaseState
         {
             SwitchState(_factory.Grab());
         }
-
-        //// Passage en state FALL
-        //if (_player.PlayerRigidbody.velocity.y < 0)
-        //{
-        //    SwitchState(_factory.Fall());
-        //}
     }
 
     public override void OnCollisionEnter2D(Collision2D collision) { }
